770b0f987aaf91a902354fd3e7116ea12fe7e0ff
Cerek Hillen <chillen@lyft.com>
2021-05-26 11:49:41 -0700

    clean up from earlier add
    Signed-off-by: Cerek Hillen <chillen@lyft.com>

--- test.py
+++
@@ -1,8 +0,0 @@
-import envoy_engine
-import envoy_requests
-import envoy_requests.common.engine
-
-
-if __name__ == "__main__":
-    envoy_requests.common.engine.Engine.log_level = envoy_engine.LogLevel.Debug
-    envoy_requests.get("https://api.lyft.com/ping")
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while true; do
-    python test.py
-    echo "Sleeping..."
-    sleep 0.1
-done
